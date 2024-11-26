b4f413abf21450c86e7a8b6a5f2e57c27fe3e57e
MarcellaDesta <marcelladesta20@gmail.com>
2024-11-26 12:27:02 +0700

    add & trash

--- resources/views/admin/newsportals/create.blade.php
+++ resources/views/admin/newsportals/create.blade.php
@@ -45,33 +45,51 @@
                                 </div>
                                 <div class="col-span-2">
                                     <label for="description"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                                    <textarea
+                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Konten
+                                    </label>
+
+                                    <div
+                                        v-for="(item, index) in items" :key="index"
+                                        class="flex">
+                                        <textarea
                                             id="description"
                                             rows="4"
                                             name="descriptions[]"
                                             v-model="x"
-                                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here">
-                                    </textarea>
+                                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            placeholder="Write product description here">
+                                        </textarea>
+                                        {{-- start div buttons --}}
+                                        <div class="flex mx-2">
+                                            <button type="button"
+                                                    @click="add">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="blue" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                  </svg>
+                                            </button>
+                                            <button type="button"
+                                                    @click="remove">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                  </svg>
+                                            </button>
+                                        </div>
+
+                                        {{-- end div buttons --}}
+
+                                    </div>
                                 </div>
+                            </div>
+
                             <div class="flex space-x-2">
                             <button
-                                    @click="increment"
-                                    type="button"
+                                    type="submit"
                                     class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                  <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                                 Increment
+                                 NewsPortal
                             </button>
-                            <p>@{{ number }}</p>
-                            <button
-                                @click="decrement"
-                                type="button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor"viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                             Decrement
-                            </button>
-
-                           </div>
-
+                              </div>
                             </div>
                         </form>
                 </div>
@@ -86,15 +104,17 @@
           setup() {
             const message = ref('Hello Gengs!');
             const x = ref('')
-
+             // deklarasi objek items
+            const items = ref({description:''});
             // mendeklarasikan varibable
             const number = ref(0);
 
-            const increment = () => {
-                number.value++
+
+            const add = () => {
+                console.log('tombol add ditekan')
             }
-            const decrement = () => {
-                number.value--
+            const remove = () => {
+                console.log('tombol remove ditekan')
 
             }
 
@@ -102,18 +122,13 @@
                     console.log(`number is ${newX}`)
                     });
 
-            // deklarasi objek items
-            const items = ref({
-                description:''
-                });
-
             return {
               message,
               items,
               x,
               number,
-              increment,
-              decrement,
+              add,
+              remove,
 
             }
           }

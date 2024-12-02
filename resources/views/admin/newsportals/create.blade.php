a848e67eba959f6d52c60fbd79520cd86650cb52
MarcellaDesta <marcelladesta20@gmail.com>
2024-12-02 10:28:05 +0700

    items

--- resources/views/admin/newsportals/create.blade.php
+++ resources/views/admin/newsportals/create.blade.php
@@ -51,12 +51,12 @@
 
                                     <div
                                         v-for="(item, index) in items" :key="index"
-                                        class="flex">
+                                        class="flex mb-2">
                                         <textarea
                                             id="description"
                                             rows="4"
                                             name="descriptions[]"
-                                            v-model="x"
+                                            v-model="item.description"
                                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="Write product description here">
                                         </textarea>
@@ -68,8 +68,10 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                                   </svg>
                                             </button>
-                                            <button type="button"
-                                                    @click="remove">
+                                            <button
+                                                    v-if="item.description.trim() == '' "
+                                                    type="button"
+                                                    @click="remove(index)">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                   </svg>
@@ -105,22 +107,26 @@
             const message = ref('Hello Gengs!');
             const x = ref('')
              // deklarasi objek items
-            const items = ref({description:''});
+            const items = ref([{description:''}]);
             // mendeklarasikan varibable
             const number = ref(0);
 
 
             const add = () => {
-                console.log('tombol add ditekan')
+                // logic untuk menambahkan items / text area
+                // console.log(items.value)
+                items.value.push({description:''});
             }
-            const remove = () => {
-                console.log('tombol remove ditekan')
+            const remove = (index) => {
+                // logic untuk menghapus items / text area
+                // console.log('tombol remove ditekan',index)
+                items.value.splice(index, 1);
 
             }
 
-            watch(number, (newX) => {
-                    console.log(`number is ${newX}`)
-                    });
+            watch(items, (items) => {
+                    console.log(`number is ${items}`)
+                    }, { deep: true });
 
             return {
               message,

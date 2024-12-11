d24c67c70c2699ac2d527329eabe8025b1fac8cc
MarcellaDesta <marcelladesta20@gmail.com>
2024-12-11 12:16:34 +0700

    11 Desember 2024

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Halo") }}, {{ auth()->user()->name }}
-                    {{ __("kamu masuk sebagai") }}, {{ auth()->user()->role }}
+                    {{-- star card galeri component --}}
+
+                    <div class="w-full max-w-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="flex mt-4 flex-col items-center pb-10">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="Bonnie image"/>
+                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex mt-4 md:mt-6">
+                                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
+                                <a href="#" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+            {{-- end card galeri component --}}
+
+                    {{-- {{ __("Halo") }}, {{ auth()->user()->name }}
+                    {{ __("kamu masuk sebagai") }}, {{ auth()->user()->role }} --}}
                 </div>
             </div>
         </div>
